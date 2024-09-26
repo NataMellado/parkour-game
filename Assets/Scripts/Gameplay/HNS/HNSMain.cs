830917c89062a7868846f3ae4e2f2db143ca9209
Cristobal Sebastian Angel Sanchez Diaz <xcrisgames@gmail.com>
2024-09-26 01:42:47 -0300

    Added main Hide n' Seek gamemode script

---
+++ Assets/Scripts/Gameplay/HNS/HNSMain.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Main class for the Hide and Seek game mode.
+/// Loop for the game mode.
+/// </summary>
+public class HNSMain : MonoBehaviour
+{
+    public static HNSMain instance;
+    // Any customization parameters for the game mode
+
+    //> Gamemode States
+
+    //> Gamemode Parameters
+
+    //> Gamemode Stats
+
+    //> Gamemode Start
+        
+    //> Gamemode Loop
+
+    //>> Start Round
+
+    //>> Round Loop
+
+    //>> End Round
+
+    //> Gamemode End
+
+}
